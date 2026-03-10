73b7fafeef6a5fdd92248399f1fa18157fd21866
krishanyadav01 <krishanyadav01k@gmail.com>
2026-03-11 01:19:51 +0530

    Update LectureTab.jsx

--- client/src/pages/admin/lecture/LectureTab.jsx
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -12,7 +12,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { toast } from 'sonner'
 
-const MEDIA_API = "http://localhost:8080/api/v1/media";
+const MEDIA_API = "https://edustack-qkzh.onrender.com/api/v1/media";
 const LectureTab = () => {
 
     const [lectureTitle, setLectureTitle] = useState("");
